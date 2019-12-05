fc67b5d173f3b575a27e95c9a3760fb657f5cb85
cedcass <cassignac.cedric@orange.fr>
2019-12-05 19:27:26 +0100

    first commit

---
+++ src/main/java/mgb/mmercato/MmercatoApplication.java
@@ -0,0 +1,11 @@
+package mgb.mmercato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmercatoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmercatoApplication.class, args);
+    }
+}
